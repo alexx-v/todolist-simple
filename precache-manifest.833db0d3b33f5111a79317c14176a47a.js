08c607651b445f60c253c46bf7457d57d2ac09a4
alexx-v <aleksandr1vlk@gmail.com>
2020-10-31 18:47:49 +0200

    Updates

---
+++ precache-manifest.833db0d3b33f5111a79317c14176a47a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ee51c1f7c29c9d960eb6ca3e95d1d1",
+    "url": "/todolist-simple/index.html"
+  },
+  {
+    "revision": "58a22d2270328eb59a26",
+    "url": "/todolist-simple/static/css/main.ddbaf20e.chunk.css"
+  },
+  {
+    "revision": "cf0dbfe9baa400a952e7",
+    "url": "/todolist-simple/static/js/2.b7a60736.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/todolist-simple/static/js/2.b7a60736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a22d2270328eb59a26",
+    "url": "/todolist-simple/static/js/main.61402344.chunk.js"
+  },
+  {
+    "revision": "a11ff6e899cb1bc86ee4",
+    "url": "/todolist-simple/static/js/runtime-main.682d6839.js"
+  }
+]);
